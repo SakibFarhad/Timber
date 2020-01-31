8967e5dc8a74a013becffed74f4ee3279e30a73a
Md. Sakib Ibne Farhad <sakib.ibne.farhad@gmail.com>
2020-02-01 02:10:28 +0600

    Project Improvement

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM ubuntu:18.04
+FROM python:3.7
 
-RUN apt-get update && \ 
-    apt-get install --no-install-recommends -y \ 
-                    python3-pip \ 
-                    python3-dev \
-                    python3-setuptools \
-                    iputils-ping \
-                    telnet && \
-    apt-get clean
+# RUN apt-get update && \ 
+#     apt-get install --no-install-recommends -y \ 
+#                     python3-pip \ 
+#                     python3-dev \
+#                     python3-setuptools \
+#                     iputils-ping \
+#                     telnet && \
+#     apt-get clean
 
 
 WORKDIR /opt
@@ -16,6 +16,6 @@ ADD ./app /opt/app/
 ADD ./glue-app /opt/glue-app/
 ADD ./requirements.txt /opt/
 
-RUN /usr/bin/python3 -m pip install -r /opt/requirements.txt
+RUN /usr/local/bin/python -m pip install -r /opt/requirements.txt
 
-ENTRYPOINT [ "/usr/bin/python3", "/opt/glue-app/mqtt.py" ] 
+ENTRYPOINT [ "/usr/local/bin/python", "/opt/glue-app/mqtt.py" ] 
---
+++ glue-app/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+from datetime import datetime
+import paho.mqtt.client as mqtt
+import pymongo
+
+mongoObj= object()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("/t/primenumber")
+    
+
+
+def on_message(client, userdata, msg):
+    payload = msg.payload 
+    print(msg.topic+" "+str(payload))
+    jsonData = json.loads(payload)
+    now = datetime.now()
+    jsonData.update({'date': now.strftime("%d/%m/%Y %H:%M")}) 
+    
+
+    print ('Connecting to mongodb...')
+    myclient = pymongo.MongoClient("mongodb://germany:germany@mongodb_container:27017/")
+    mydb = myclient["primedb"]
+    mycol = mydb["primecollection"]
+    print ('Inserting Data..')
+    print(mycol.insert_one(jsonData).inserted_id)
+    
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt_container", 1883, 60)
+
+client.loop_forever()
